326e21e690381802a2061ed162b3812e3a352fc2
Taha Ilyas <ilyasmayo14@gmail.com>
2026-08-28 02:32:13 +0500

    Prepare frontend for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,8 @@ app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
 
 
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
